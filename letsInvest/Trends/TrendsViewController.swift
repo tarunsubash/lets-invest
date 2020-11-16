60b88052a7ea8d6c2da0d16eec137e37f1e45f21
Subash <tarun.subash01@gmail.com>
2020-11-16 12:35:08 +0530

    remove unused code

--- letsInvest/Trends/TrendsViewController.swift
+++ letsInvest/Trends/TrendsViewController.swift
@@ -13,23 +13,5 @@ class TrendsViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .clear
         title = "Trending"
-        createHorizontalScrollView()
-    }
-    
-    func createHorizontalScrollView() {
-        let scrollView = UIScrollView(frame: CGRect(x: 0, y: 80, width: self.view.frame.size.width, height: 60))
-        
-        var buttonX: Int = 0
-        
-        for i in 0...5 {
-            var button = UIButton(frame: CGRect(x: buttonX, y: 0, width: 100, height: 60))
-            button.setTitle("Button \(i)", for: .normal)
-            scrollView.addSubview(button)
-            button.tintColor = .white
-            buttonX = buttonX + 100
-        }
-        scrollView.contentSize = CGSize(width: buttonX, height: 60)
-        scrollView.backgroundColor = .cyan
-        view.addSubview(scrollView)
     }
 }
